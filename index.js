1a0ee3d47c8bce26b91f5d972fd03b9eb90f4bec
Joshua Thomas Wise <joshua.thomaswise@vice.com>
2018-01-30 21:28:37 -0500

    improve robustness

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 'use strict';
 const Result = require('./lib/result');
+const clean = require('./lib/clean');
 const async = require('./lib/async');
 const sync = require('./lib/sync');
 
-const makeAsync = (fn) => (cb) => { fn(); cb(); };
+const makeAsync = fn => cb => { fn(); cb(); };
 const noopSync = () => {};
-const noopAsync = cb => cb();
+const noopAsync = cb => { cb(); };
 
 module.exports = (fn, setup, duration) => {
-  if (typeof fn !== 'function') throw new TypeError('Expected benchmark subject to be a function');
-  if (setup == null) setup = fn.length ? noopAsync : noopSync;
-  if (duration == null) duration = 3000;
-  if (typeof setup !== 'function') throw new TypeError('Expected benchmark setup to be a function');
-  if (!Number.isInteger(duration)) throw new TypeError('Expected benchmark duration to be an integer');
-  if (!fn.length && !setup.length) return new Result(sync(fn, setup, duration));
-  if (!fn.length) fn = makeAsync(fn);
-  if (!setup.length) setup = makeAsync(setup);
-  let callback;
-  const promise = new Promise((rs, rj) => { callback = (e, s) => e ? rj(e) : rs(new Result(s)); });
-  async(fn, setup, duration, callback);
-  return promise;
+	if (typeof fn !== 'function') throw new TypeError('Expected benchmark subject to be a function');
+	if (setup == null) setup = fn.length ? noopAsync : noopSync;
+	if (duration == null) duration = 3000;
+	if (typeof setup !== 'function') throw new TypeError('Expected benchmark setup to be a function');
+	if (!Number.isInteger(duration)) throw new TypeError('Expected benchmark duration to be an integer');
+	if (!fn.length && setup.length) fn = makeAsync(fn);
+	if (!setup.length && fn.length) setup = makeAsync(setup);
+	
+	// Benchmark the benchmarker, so we can factor it out later.
+	const noop = fn.length ? noopAsync : noopSync;
+	const noise = benchmark(noop, noop, Math.min(Math.round(duration / 10), 300));
+	
+	// Then run the actual benchmark.
+	if (!fn.length) return benchmark(fn, setup, duration, noise);
+	return noise.then(noise => benchmark(fn, setup, duration, noise));
+};
+
+const benchmark = (fn, setup, duration, noise) => {
+	if (!fn.length) return new Result(clean(sync(fn, setup, duration), noise));
+	let callback;
+	const promise = new Promise((resolve, reject) => { callback =
+		(err, samples) => err ? reject(err) : resolve(new Result(clean(samples, noise))); });
+	async(fn, setup, duration, callback);
+	return promise;
 };
