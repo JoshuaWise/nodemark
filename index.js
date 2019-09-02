8aa1b354540777a505414facc1603aa193591fb1
Eric Breno <eric.santos@ccc.ufcg.edu.br>
2019-09-02 00:21:00 -0300

    Adding runs parameter to index

--- index.js
+++ index.js
@@ -2,15 +2,19 @@
 const benchmark = require('./lib/benchmark');
 const makeAsync = fn => cb => { fn(); cb(); };
 
-module.exports = (fn, setup, duration) => {
+module.exports = (fn, setup, duration, runs = null) => {
 	if (typeof fn !== 'function') throw new TypeError('Expected benchmark subject to be a function');
 	if (setup == null) setup = fn.length ? cb => { cb(); } : () => {};
 	if (duration == null) duration = 3300;
 	if (typeof setup !== 'function') throw new TypeError('Expected benchmark setup to be a function');
 	if (!Number.isInteger(duration)) throw new TypeError('Expected benchmark duration to be an integer');
   if (duration <= 0) throw new TypeError('Expected benchmark duration to be positive');
+	if (duration !== null) {
+		if (!Number.isInteger(duration)) throw new TypeError('Expected benchmark total runs to be an integer');
+		if (duration <= 0) throw new TypeError('Expected benchmark total runs to be positive');
+	}
 	if (!fn.length && !setup.length) return benchmark.sync(fn, setup, duration);
 	if (!fn.length) fn = makeAsync(fn);
 	if (!setup.length) setup = makeAsync(setup);
-	return benchmark.async(fn, setup, duration);
+	return benchmark.async(fn, setup, duration, runs);
 };
